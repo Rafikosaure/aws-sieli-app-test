dcde7a61e1492b6cd70bcc1b0625c0b25d53fba3
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-07-17 16:40:20 +0200

    mise à jour du fichier config.js

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const env = {
-  port: 8000,
-  token: '0m3rIJ3RHIPDBjqtrAQrYxZwDuYwlv1tOzbW9LeSBPt1gGe7Lc'
+  port: process.env.PORT,
+  token: process.env.TOKEN
 }
